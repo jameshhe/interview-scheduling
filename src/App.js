fd679fc934cade78edd5f761120b73149aaa68b8
Eli <elisego46@gmail.com>
2021-10-20 17:26:30 -0500

    Update nav-bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <a>Home</a>
-        <a>Schedule</a>
-      </nav>
-      <button>New Button!</button>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <nav>
+                <a>Home</a>
+                <a>Schedule</a>
+                <a>Resources</a>
+            </nav>
+            <button>New Button!</button>
+        </div>
+    );
 }
 
 export default App;
