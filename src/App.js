a46c6d1cf0504641d2b30ddc91039bd455dca2a4
drew-harris <dsharris10@gmail.com>
2021-10-20 17:59:24 -0500

    fix(indent): fix inconsistent indentation in App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className='App'>
-            <nav>
-                <a href='#'>Home</a>
-                <a href='#'>Schedule</a>
-                <a href='#'>Resources</a>
-            </nav>
-            <button>New Button!</button>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <nav>
+        <a href='#'>Home</a>
+        <a href='#'>Schedule</a>
+        <a href='#'>Resources</a>
+      </nav>
+      <button>New Button!</button>
+    </div>
+  );
 }
 
 export default App;
