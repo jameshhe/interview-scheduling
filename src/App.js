daf106dc9da03025ca7252b8d5003845a801b14f
jameshhe <jameshuang@smu.edu>
2021-10-20 17:20:11 -0500

    add(nav): added home and schedule to nav bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <nav>
+        <a>Home</a>
+        <a>Schedule</a>
+      </nav>
       <button>New Button!</button>
     </div>
   );
